e3cc63201a38aad26e41e19a5b29ee7041697757
ngoctd <trangoc.contact@gmail.com>
2022-12-28 09:22:28 +0700

    add execute in goroutine

--- signal.go
+++ signal.go
@@ -8,25 +8,26 @@ import (
 
 var nTo1Notify sync.WaitGroup
 
-// ExecuteBeforeQuit execute input function
-// WARN: this function is blocking operation, should use with goroutine
-func ExecuteBeforeQuit(fn func()) {
+// ExecBeforeShutDown execute input function
+func ExecBeforeShutDown(fn func()) {
 	nTo1Notify.Add(1)
 
-	sig := make(chan os.Signal)
-	signal.Notify(sig, os.Interrupt)
+	go func() {
+		sig := make(chan os.Signal)
+		signal.Notify(sig, os.Interrupt)
 
-	select {
-	// Execute before quit
-	case <-sig:
-		defer nTo1Notify.Done()
-		fn()
-	}
+		select {
+		// Execute before quit
+		case <-sig:
+			defer nTo1Notify.Done()
+			fn()
+		}
+	}()
 }
 
 // Wait execute input function and wait all sub task to be executed
 func Wait(fn func()) {
-	ExecuteBeforeQuit(fn)
+	ExecBeforeShutDown(fn)
 
 	nTo1Notify.Wait()
 }
